d5a24bd9a7c7493794a9969eec5d74ecd9d20a3c
Sanghee Kim <sangheestyle@gmail.com>
2016-04-05 17:55:51 -0400

    Adding filters

--- board/views.py
+++ board/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import get_user_model
 
-from rest_framework import authentication, permissions, viewsets
+from rest_framework import authentication, permissions, viewsets, filters
 
+from .forms import TaskFilter, SprintFilter
 from .models import Sprint, Task
 from .serializers import SprintSerializer, TaskSerializer, UserSerializer
 
@@ -23,6 +24,11 @@ class DefaultsMixin(object):
     paginate_by = 25
     paginate_by_param = 'page_size'
     max_paginate_by = 100
+    filter_backends = (
+        filters.DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    )
 
 
 class SprintViewSet(DefaultsMixin, viewsets.ModelViewSet):
@@ -30,6 +36,9 @@ class SprintViewSet(DefaultsMixin, viewsets.ModelViewSet):
 
     queryset = Sprint.objects.order_by('end')
     serializer_class = SprintSerializer
+    filter_class = SprintFilter
+    search_fields = ('name', )
+    ordering_fields = ('end', 'name', )
 
 
 class TaskViewSet(DefaultsMixin, viewsets.ModelViewSet):
@@ -37,6 +46,9 @@ class TaskViewSet(DefaultsMixin, viewsets.ModelViewSet):
 
    queryset = Task.objects.all()
    serializer_class = TaskSerializer
+   filter_class = TaskFilter
+   search_fields = ('name', 'description', )
+   ordering_fields = ('name', 'order', 'started', 'due', 'completed', )
 
 
 class UserViewSet(DefaultsMixin, viewsets.ReadOnlyModelViewSet):
@@ -46,4 +58,5 @@ class UserViewSet(DefaultsMixin, viewsets.ReadOnlyModelViewSet):
    lookup_rul_kwarg = User.USERNAME_FIELD
    queryset = User.objects.order_by(User.USERNAME_FIELD)
    serializer_class = UserSerializer
+   search_fields = (User.USERNAME_FIELD, )
 
