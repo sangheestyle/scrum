a277e74520756ad0d24c4e4a42cb4b9b70a491db
Sanghee Kim <sangheestyle@gmail.com>
2016-04-05 15:45:25 -0400

    Connecting to the Router

--- scrum/urls.py
+++ scrum/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 from rest_framework.authtoken.views import obtain_auth_token
 
+from board.urls import router
+
 
 urlpatterns = [
     url(r'^api/token/', obtain_auth_token, name='api-token'),
+    url(r'^api/', include(router.urls)),
 ]
