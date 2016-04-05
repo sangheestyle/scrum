02d2688f0c11100dfd1c9511fa58fef1dc16a026
Sanghee Kim <sangheestyle@gmail.com>
2016-04-05 15:21:55 -0400

    sprint endpoint

--- board/views.py
+++ board/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import authentication, permissions, viewsets
 
-# Create your views here.
+from .models import Sprint
+from .serializers import SprintSerializer
+
+
+class DefaultsMixin(object):
+    """Default settings for view authentication, permissions, filtering
+    and pagination."""
+
+    authentication_classes = (
+        authentication.BasicAuthentication,
+        authentication.TokenAuthentication,
+    )
+    permission_class = (
+        permissions.IsAuthenticated,
+    )
+    paginate_by = 25
+    paginate_by_param = 'page_size'
+    max_paginate_by = 100
+
+
+class SprintViewSet(DefaultsMixin, viewsets.ModelViewSet):
+    """API endpoint for listing and creating sprints."""
+
+    queryset = Sprint.objects.order_by('end')
+    serializer_class = SprintSerializer
